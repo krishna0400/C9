4ecfae7351200a3c528e4f6471bae756bcab9f4e
krishna0400 <89008718+krishna0400@users.noreply.github.com>
2021-10-05 14:55:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var box
+
+function setup() {
+  createCanvas(400, 400);
+  box=createSprite(200,200,30,30);
+}
+
+function draw() {
+  background(0);
+if (keyIsDown(RIGHT_ARROW)){
+  box.position.x=box.position.x+5;
+}
+
+  drawSprites();
+}
